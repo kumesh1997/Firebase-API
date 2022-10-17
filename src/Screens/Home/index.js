bcad838dc150068af25c99f665906b7ae3789a47
kumesh1997 <tharindukumesh09@gmail.com>
2022-10-17 16:13:37 +0530

    Add student is completed

---
+++ src/Screens/Home/index.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import { useEffect } from "react";
+import { db } from "../../firebase-config";
+import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
+import { Link, useNavigate } from 'react-router-dom';
+
+function Home() {
+
+  let history = useNavigate();
+  const [student, setStudent] = useState([]);
+  const studentsCollectionReference = collection(db, "students");
+  
+   // fetchb data when page is rendering
+   useEffect(() => {
+    const getStudent = async () => {
+    const data = await getDocs(studentsCollectionReference);
+    setStudent(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    }
+    getStudent();
+  }, []);
+
+   // delete a document
+   const deleteHandler = async ( stdId ) => {
+    const studentDoc = doc(db, "students", stdId )
+    await deleteDoc(studentDoc);
+    history("/");
+  }
+
+  return (
+    <div>
+      <div><Link to={'/modal'}><button type="button" class="btn btn-primary">Add New Student</button></Link></div>
+      <table class="table table-dark table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Address</th>
+      <th scope="col">Birth Day</th>
+      <th scope="col">GPA</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  {
+      student.map((std,key) => {
+        return <tr key={key+1}>
+          <th scope="row">{key}</th>
+          <td>{std.Name}</td>
+          <td>{std.Address}</td>
+          <td>{std.DOB}</td>
+          <td>{std.GPA}</td>
+          <td>
+            <Link to={'/edit'}>
+              <button type="button" class="btn btn-primary" >Update</button>
+            </Link>
+          &nbsp;
+          <button type="button" class="btn btn-danger" onClick={() => {deleteHandler(std.id)}}>Delete</button>
+          </td>
+          
+        </tr>
+      })
+     }
+  </tbody>
+</table>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/Screens/UpdateStudents/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useState } from "react";
+import { db } from "../../firebase-config";
+import { addDoc, collection, deleteDoc, doc, getDocs, updateDoc } from "firebase/firestore";
+import { Link } from 'react-router-dom';
+
+
+function UpdateStudents() {
+
+  // reference to the collection
+  const studentsCollectionReference = collection(db, "students");
+
+  // consts
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [dob, setDob] = useState("");
+  const [gpa, setGpa] = useState(0);
+
+   // handle on submit
+   const handleOnSubmit = async () => {
+    let data = await addDoc(studentsCollectionReference, {Address: address, DOB: dob, GPA: gpa, Name: name })
+  }
+
+  return (
+    <div>
+         <form >
+            <input placeholder="Name..."  onChange={(e) => {setName(e.target.value)}}/>
+            <input placeholder="Address"  onChange={(e) => {setAddress(e.target.value)}}/>
+            <input placeholder="Birth Day"  onChange={(e) => {setDob(e.target.value)}}/>
+            <input placeholder="GPA"  onChange={(e) => {setGpa(e.target.value)}}/>
+            <div>
+            <Link to={'/'}><button type="button" class="btn btn-danger">Cancel</button></Link>
+              &nbsp;
+            <Link to={'/'}><button type="button" class="btn btn-success" onClick={handleOnSubmit} >Save</button></Link>
+            </div>
+            </form>
+    </div>
+  )
+}
+
+export default UpdateStudents;
